79e7e28fb9cd61246095a44056900a543fe50eca
hadidi09 <cheikhhadramy@yahoo.fr>
2022-01-18 18:23:04 +0100

    responsive resolve

--- pages/accueil.js
+++ pages/accueil.js
@@ -14,20 +14,25 @@ import neon from "../src/buzz.mp3"
 
 const Accueil = () =>
 {
-    
+    const [toggle, setToggle] = useState(false)
+    const menuToggle = () =>
+    {
+        setToggle(!toggle)
+        console.log(toggle);
+    }
 
     return (
         <div className='bg-black'>
             <header>
                 <nav className='text-white'>
                     <ul className='flex flex-row justify-between mx-2'>
-                        <li> <FontAwesomeIcon icon={faBars} size="2x" className='mt-2 cursor-pointer' /></li>
+                        <li> <FontAwesomeIcon icon={faBars} size="2x" onClick={menuToggle} className='mt-2 cursor-pointer' /></li>
                         {/* <li>S'inscrire sur les listes électorales</li> */}
                     </ul>
                 </nav>
             </header>
             <main className='text-white w-screen h-screen flex justify-center flex-col items-center'>
-                <h1 className=' text-5xl md:text-8xl lg:text-9xl'>
+                <h1 className=' text-5xl md:text-8xl lg:text-9xl z-50'>
                     <span className='suff'>SUFF</span>
                     <span className='rage'>RAGE</span>
                 </h1>
@@ -40,9 +45,9 @@ const Accueil = () =>
                             <code>audio</code> element.
                     </audio>
                
-                    <p className=' mt-12 font-bold text-xs sm:text-sm md:text-md  '>S'inscrire sur les listes électorales</p>
+                    <p className=' mt-12 font-bold text-xs sm:text-sm md:text-md z-50  '>S'inscrire sur les listes électorales</p>
                 
-                <div className=' absolute bottom-2 animate-bounce  '>
+                <div className=' absolute bottom-2 animate-bounce z-50 '>
                     <button>
                         <Link href="/menu">
                          <a><FontAwesomeIcon icon={faAngleUp} size="3x" /></a>
@@ -50,6 +55,24 @@ const Accueil = () =>
                     </button>
                      
                 </div>
+                {!toggle ? null : (
+                    <>
+                        <section className='absolute top-2 bottom-2 left-2  pl-3 cursor-pointer '>
+                            <nav>
+                                <ul className='flex flex-col '>
+                                    <li> <FontAwesomeIcon icon={faBars} onClick={menuToggle} size="2x" className='mt-3 cursor-pointer' />
+                                        <span className=' font-bold text-3xl  py-5 ml-2 '>Menu</span> </li>
+                                    <li className=' liste_titre font-bold text-sm py-5 '>Qui sommes nous ?</li>
+                                    <li className=' liste_titre font-bold text-sm py-5 '>Nos actions</li>
+                                    <li className=' liste_titre font-bold text-sm py-5 '>Nous rejoindre</li>
+                                    <li className=' liste_titre contact font-bold text-sm py-5  '>Contacts</li>
+                                </ul>
+                            </nav>
+                        </section>
+            
+                    </>
+                )  }
+              
                
             </main>
         </div>
